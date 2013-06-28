5c2f7a413fb2f67639a53c2a2859b0c75fe7dc92
Robert Boyd <rboyd@Roberts-MacBook-Air.local>
2013-06-27 23:03:01 -0500

    adding dotfiles. DRY this up when I learn puppet script.

--- modules/people/manifests/rboyd.pp
+++ modules/people/manifests/rboyd.pp
@@ -11,4 +11,41 @@ class people::rboyd {
   include alfred
   include vagrant
   include virtualbox
+
+  $home = "/Users/${::boxen_user}"
+  $dotfiles_dir = "${boxen::config::srcdir}/dotfiles"
+
+  repository { $dotfiles_dir:
+    source => "http://github.com/rboyd/dotfiles"
+  }
+
+  file { "${home}/.zshrc":
+    ensure  => link,
+    target  => "${dotfiles_dir}/.zshrc",
+    require => Repository[$dotfiles_dir]
+  }
+
+  file { "${home}/.tmux.conf":
+    ensure  => link,
+    target  => "${dotfiles_dir}/.tmux.conf",
+    require => Repository[$dotfiles_dir]
+  }
+
+  file { "${home}/.slate":
+    ensure  => link,
+    target  => "${dotfiles_dir}/.slate",
+    require => Repository[$dotfiles_dir]
+  }
+
+  file { "${home}/.emacs":
+    ensure  => link,
+    target  => "${dotfiles_dir}/.emacs",
+    require => Repository[$dotfiles_dir]
+  }
+  
+  file { "${home}/.emacs.d":
+    ensure  => link,
+    target  => "${dotfiles_dir}/.emacs.d",
+    require => Repository[$dotfiles_dir]
+  }
 }
